fe3ef2d79ac775012e1941e168983a64107da441
fenghe <jxphxufh@gmail.com>
2019-06-04 01:39:49 +0800

    refactor: add Crawler class

--- crawler.py
+++ crawler.py
@@ -2,34 +2,38 @@ import urllib.request
 from bs4 import BeautifulSoup
 import ssl
 
+class Crawler:
+    def _crawl_html(self, uri):
+        # Ignore SSL certificate errors
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
 
+        html = urllib.request.urlopen(uri, context=ctx).read()
+        return html
 
-# path: relative path of the uri
-def crawl_html(uri):
-    # Ignore SSL certificate errors
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
+    # this function saves a latex string as a .latex file
+    def _save_latex(self, latex_doc, filename):
+        with open('data/{0}.latex'.format(filename), 'w') as out:
+            out.write(latex_doc)
 
-    html = urllib.request.urlopen(uri, context=ctx).read()
-    return html
+    # this functions crawls a single page LaTex file.
+    # uri: the uri of the page that contains latex
+    # filename: the name of the file to be saved
+    def crawl_single_page_latex(self, uri, filename):
+        # beautiful soup
+        html = self._crawl_html(uri)
 
+        soup = BeautifulSoup(html, 'html.parser')
 
-# this functions crawls a single page LaTex file.
-def crawl_single_page_latex(uri):
-    # beautiful soup
-    html = crawl_html(uri)
+        # get the plain latex
+        latex = soup.find('div', class_="highlight").text
 
-    soup = BeautifulSoup(html, 'html.parser')
+        self._save_latex(latex, filename)
 
-    latex = soup.find('div', class_="highlight").text
-
-    return latex
-
-# this function saves a latex string as a .tex file
-def save_latex(latex)
-    pass
-
+    def block_test(self):
+        crawler.crawl_single_page_latex('http://www.texample.net/tikz/examples/city/', 'city')
 
 if __name__ == '__main__':
-    print(crawl_single_page_latex('http://www.texample.net/tikz/examples/city/'))
+   crawler = Crawler()
+   crawler.block_test()
